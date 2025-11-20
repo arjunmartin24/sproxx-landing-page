0de61339419499dfa57d0f53f62b1656ec4fb59d
ARJUN G <209875913+arjunmartin24@users.noreply.github.com>
2025-11-20 15:36:17 -0500

    Add safelist to Tailwind CSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,13 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  safelist: [
+    "bg-gradient-to-r",
+    "bg-clip-text",
+    "text-transparent",
+    "from-[#7C4DFF]",
+    "via-[#4FC3F7]",
+    "to-[#00E5FF]",
+  ],
   theme: {
     extend: {
       colors: {
